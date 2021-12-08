25ae1f11c450f9e7eba11b4443c418a9f11c7bf3
zzh611 <48823712+zzh611@users.noreply.github.com>
2021-12-07 22:22:02 -0600

    update shiny

--- code/Yelp_analysis/ui.R
+++ code/Yelp_analysis/ui.R
@@ -1,15 +1,19 @@
-shinyUI(fluidPage(
-    includeCSS("www/bootstrap.css"),
+shinyUI(
+    fluidPage(theme = shinytheme("simplex"),
 
     # Application title
-    titlePanel("Yelp BBQ Analysis"),
+    titlePanel(fluidPage(
+        column(img(src="yelp.png",width="200px"),width=2),
+        column(br(),"BBQ Restaurant Analysis",width=8)
+    )
+    ),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
                   radioButtons("inputway", "Find Your BBQ Restaurant:",
-                               list("Business ID" = "bid",
-                                    "Business Name" = "bname")),
+                               list("Business Name" = "bname",
+                                    "Business ID" = "bid")),
                   conditionalPanel(condition="input.inputway=='bid'",
                                    textInput("id","Business ID:",value = "")),
                   conditionalPanel(condition="(input.inputway=='bname')",
@@ -30,47 +34,56 @@ shinyUI(fluidPage(
             tabPanel("Info",h2(strong(textOutput("nametitle"))),
                      conditionalPanel(condition="input.submit",
                      fluidRow(column(6,
-                                     fluidRow(column(3,HTML("<div style='height: 20px;'>"),
+                                     fluidRow(column(4,HTML("<div style='height: 20px;'>"),
                                                      imageOutput("star"),
                                                      HTML("</div>")),
                                               column(5,div(textOutput("reviewcount")))),
                                      h3("Location"),
-                                     h5(textOutput("address"),style = "color:grey"),
+                                     h5(textOutput("address")),
                                      tags$hr(),
                                      h3("Amenities"),
-                                     fluidRow(column(1,HTML("<div style='height: 20px;'>"),
+                                     fluidRow(column(2,HTML("<div style='height: 20px;'>"),
                                                      imageOutput("takeouticon"),
                                                      HTML("</div>")),
                                               div(textOutput("takeout")),
                                               br(),
-                                              column(1,HTML("<div style='height: 20px;'>"),
+                                              column(2,HTML("<div style='height: 20px;'>"),
                                                      imageOutput("deliveryicon"),
                                                      HTML("</div>")),
                                               div(textOutput("delivery")),
                                               br(),
-                                              column(1,HTML("<div style='height: 20px;'>"),
+                                              column(2,HTML("<div style='height: 20px;'>"),
                                                      imageOutput("parkingicon"),
                                                      HTML("</div>")),
                                               div(textOutput("parking")),
                                               br(),
-                                              column(1,HTML("<div style='height: 20px;'>"),
+                                              column(2,HTML("<div style='height: 20px;'>"),
                                                      imageOutput("outdoorseatingicon"),
                                                      HTML("</div>")),
                                               div(textOutput("outdoorseating"))
                                               ),
-                                     
+                                     tags$hr(), 
+                                     h3("Other Attributes"), 
+                                     h4("Noise"), h5(textOutput("noise")),
                                      tags$hr(),
-                                     h3("Other Attributes"),
-                                     h4("Noise"),
-                                     h5(textOutput("noise"),style = "color:grey"),
-                                     tags$hr(),
-                                     h4("Alcohol"),
-                                     h5(textOutput("alcohol"),style = "color:grey"),
+                                     h4("Alcohol"), h5(textOutput("alcohol"))
                                      
-                                     ))
+                                     ),column(6,leafletOutput("map",height=400)))
             )),
-            tabPanel("Compare"),
-            tabPanel("Good & Bad")
+            tabPanel("Like",
+                     fluidRow(
+                         column(12,align="center",HTML("<div style='height: 89px;'>"),
+                                conditionalPanel(condition="input.submit",h3("Positive Reviews")),
+                                HTML("</div>"),
+                                wordcloud2Output('pos')))),
+            tabPanel("Dislike",
+                     fluidRow(
+                         column(12,align="center",HTML("<div style='height: 89px;'>"),
+                                conditionalPanel(condition="input.submit",h3("Negative Reviews")),
+                                HTML("</div>"),
+                                wordcloud2Output('neg')))),
+            tabPanel("Competitors"),
+            tabPanel("Suggestions"),
             
         )
     )
